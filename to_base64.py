c8eba98c7da3be5b94de909fb84ece2b7d7fa6a8
mai19950 <underdog-95@outlook.com>
2023-04-07 19:06:40 +0800

    feat: add to_base64.py file

---
+++ to_base64.py
@@ -0,0 +1,11 @@
+import base64
+
+def convert_to_base64():
+  proxy_list = open('./proxy.txt', mode="r", encoding="utf8")
+  with open('./proxy_base64.txt', mode="wb+") as f:
+    f.write(base64.b64encode(bytes(proxy_list.read(), 'utf-8')))
+    proxy_list.close()
+    
+
+if __name__ == '__main__':
+  convert_to_base64()
